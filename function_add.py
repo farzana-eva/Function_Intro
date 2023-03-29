233c08719cda31e0f2d62251a490cd0099f38ffc
Farzana Eva <farzanaeva@Farzanas-iMac.local>
2023-03-28 23:15:50 -0400

    add function added

---
+++ function_add.py
@@ -0,0 +1,12 @@
+def adding(x, y, z):
+    result = x + y + z
+    return result
+
+
+answer = adding(4, 6, 9)
+print(answer)
+
+for val in range(1, 5):
+    adding_value = adding(2, val, 5)
+    print(adding_value)
+
