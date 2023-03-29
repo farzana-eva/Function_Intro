4c33c6545feac355f2579aabd58e944af4bee1a8
Farzana Eva <farzanaeva@Farzanas-iMac.local>
2023-03-29 18:59:43 -0400

    palindrome.py file added

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+
+def is_palindrome(string):
+    #  backwards = string[::-1]
+    #  return backwards == string
+    return string[::-1].casefold() == string.casefold()
+
+
+word = input("Please enter a word to check: ")
+if is_palindrome(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
+
